e94a5eab5d0583cec1376ebfacdbd7c74405edbb
iicba <95548021+iicba@users.noreply.github.com>
2022-07-07 20:25:30 -0400

    Basic Rendering of Home Screen

--- adventure-wise/src/App.js
+++ adventure-wise/src/App.js
@@ -1,5 +1,12 @@
+import React from 'react';
+import Home from  '/Users/anthonyclay/coding/Adventure-Wise/adventure-wise/src/home/Home.js';
+
 function App() {
-   return <div className="App"></div>;
+   return (
+   <div className="App">
+      <Home />
+   </div>
+      );
 }
 
 export default App;
