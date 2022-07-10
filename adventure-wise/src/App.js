ae2b4b6dcdc5c0ca16a5fc5826ae604d61c78fe9
Yanchao Jin <yanchaojin@YanchaodeiMac.local>
2022-07-10 00:15:58 -0500

    first push

--- adventure-wise/src/App.js
+++ adventure-wise/src/App.js
@@ -1,9 +1,12 @@
 import HeaderApp from "./header/HeaderApp";
 import React from "react";
+import Navbar from "./navbar/navbar";
 
 function App() {
    return (
           <div className="App">
+              
+              <Navbar />
               <HeaderApp />
           </div>
    );
---
+++ adventure-wise/src/navbar/navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './navbar.css';
+
+function Navbar(){
+   return(
+    <div className="navbar">
+        <button class="button" >HOME</button>
+        <button class="button" >EXPLORE</button>
+        <button class="button" >MY TRIPS</button>
+    </div>    
+   
+   )
+
+}
+
+export default Navbar;
