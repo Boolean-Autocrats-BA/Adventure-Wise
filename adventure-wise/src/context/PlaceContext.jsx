653f2f5b85035ec13b4b4fe4dbf8c652bebead1d
Michael Howard <mike.gene.howard@gmail.com>
2022-07-11 12:32:03 -1000

    added state to context api, made component more dynamic

--- adventure-wise/src/context/PlaceContext.jsx
+++ adventure-wise/src/context/PlaceContext.jsx
@@ -3,12 +3,16 @@ import { useState, createContext } from "react";
 const PlaceContext = createContext();
 
 export const PlaceProvider = ({ children }) => {
-  //state
+  /* state used for myTrips component -Mike */
+  // Dummy data to fill trip details in myTrips component -Mike
   const [places, setPlaces] = useState([
-    { id: 1, place: "Hawaii" },
-    { id: 2, place: "Paris" },
-    { id: 3, place: "Hong Kong" },
+    { id: 1, place: "Hawaii", dates: "12 - 19 Aug 22", events: 10 },
+    { id: 2, place: "Paris", dates: "1 - 10 Sep 22", events: 15 },
+    { id: 3, place: "Hong Kong", dates: "15 - 31 Oct 22", events: 12 },
   ]);
+
+  // Used to determine if there's data to render to trip details in myTrips component -Mike
+  const [isTripAdded, setTripAdded] = useState(false);
 
   const addPlace = (place) => {
     setPlaces([...places, place]);
@@ -19,6 +23,8 @@ export const PlaceProvider = ({ children }) => {
       value={{
         places,
         addPlace,
+        isTripAdded,
+        setTripAdded,
       }}
     >
       {children}
--- adventure-wise/src/my-trips/components/TripList.jsx
+++ adventure-wise/src/my-trips/components/TripList.jsx
@@ -1,9 +1,15 @@
+import { useContext } from "react";
+import PlaceContext from "../../context/PlaceContext";
+import TripListItem from "./TripListItem";
+
 const TripList = () => {
+  const { places } = useContext(PlaceContext);
+
   return (
     <div className="trip_listItems">
-      <button className="trip_btn">Trip</button>
-      <button className="trip_btn">Trip</button>
-      <button className="trip_btn">Trip</button>
+      {places?.map((place) => (
+        <TripListItem key={place.id} place={place} />
+      ))}
     </div>
   );
 };
