c121557240785f298e2d7d9c26295e40ae4b8e2d
magdielrosario97 <magdielrosario97@gmail.com>
2022-07-15 11:03:34 -0500

    fixed double header

--- adventure-wise/src/context/UserContext.js
+++ adventure-wise/src/context/UserContext.js
@@ -3,27 +3,27 @@ import { createContext, useState } from "react";
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [loggedIn, setLoggedIn] = useState(true);
-  const [userID, setUser] = useState(8);
-  const [userProfile, setUserProfile] = useState(null);
-  const [userTrips, setUserTrips] = useState(null);
+   const [loggedIn, setLoggedIn] = useState(false);
+   const [userID, setUser] = useState(null);
+   const [userProfile, setUserProfile] = useState(null);
+   const [userTrips, setUserTrips] = useState(null);
 
-  return (
-    <UserContext.Provider
-      value={{
-        loggedIn,
-        setLoggedIn,
-        userID,
-        setUser,
-        userProfile,
-        setUserProfile,
-        userTrips,
-        setUserTrips,
-      }}
-    >
-      {children}
-    </UserContext.Provider>
-  );
+   return (
+      <UserContext.Provider
+         value={{
+            loggedIn,
+            setLoggedIn,
+            userID,
+            setUser,
+            userProfile,
+            setUserProfile,
+            userTrips,
+            setUserTrips,
+         }}
+      >
+         {children}
+      </UserContext.Provider>
+   );
 };
 
 export default UserContext;
