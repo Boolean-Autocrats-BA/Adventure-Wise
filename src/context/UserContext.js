85649968b41128e6ea3652cbf34c20a0adc3f93a
magdielrosario97 <magdielrosario97@gmail.com>
2022-07-18 16:17:42 -0500

    added process.env key

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -11,7 +11,7 @@ export const UserProvider = ({ children }) => {
    const [searchRes, setSearchRes] = useState(null);
 
    const { isLoaded } = useLoadScript({
-      googleMapsApiKey: ,
+      googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
       libraries: ["places"],
    });
 
@@ -27,7 +27,7 @@ export const UserProvider = ({ children }) => {
             userTrips,
             setUserTrips,
             searchRes,
-            setSearchRes
+            setSearchRes,
          }}
       >
          {children}
