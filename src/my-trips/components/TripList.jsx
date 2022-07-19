0bfcb806e32a8a7444e541d2d8c2fe5154835f99
Siddhant Bose <102205829+pyrotechnicsid@users.noreply.github.com>
2022-07-19 10:49:14 -0500

    Revert "feature complete"

--- src/my-trips/components/TripList.jsx
+++ src/my-trips/components/TripList.jsx
@@ -1,73 +1,21 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import UserContext from "../../context/UserContext";
 import TripListItem from "./TripListItem";
-import Modal from "react-modal";
-import AddTripModal from "./AddTripModal";
-import { BsPlusLg } from "react-icons/bs";
-
-Modal.setAppElement("#root");
-
-const customStyles = {
-   content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-      borderRadius: "10px",
-   },
-};
 
 const TripList = () => {
-   const { userTrips } = useContext(UserContext);
-   const [addTrip, setAddTrip] = useState(false);
-   const [modalIsOpen, setIsOpen] = useState(false);
+  const { userTrips } = useContext(UserContext);
 
-   let subtitle;
-
-   function openModal() {
-      setIsOpen(true);
-   }
-
-   function afterOpenModal() {
-      // references are now sync'd and can be accessed.
-      subtitle.style.color = "black";
-   }
-
-   function closeModal() {
-      setIsOpen(false);
-   }
-
-   return (
-      <>
-         <div className="trip_listItems">
-            <button id="addTripBtn" onClick={openModal}>
-               <BsPlusLg />
-            </button>
-            {userTrips?.map((trip) => (
-               <TripListItem key={trip.trip_id} tripId={trip.trip_id} tripName={trip.trip_name} />
-            ))}
-         </div>
-         <Modal
-            isOpen={modalIsOpen}
-            onAfterOpen={afterOpenModal}
-            onRequestClose={closeModal}
-            style={customStyles}
-            contentLabel="Login Modal"
-         >
-            <div className="modalContainer">
-               <h2 id="modalHeader" ref={(_subtitle) => (subtitle = _subtitle)}>
-                  create trip
-               </h2>
-               <button id="modalCloseBtn" onClick={closeModal}>
-                  X
-               </button>
-               <AddTripModal />
-            </div>
-         </Modal>
-      </>
-   );
+  return (
+    <div className="trip_listItems">
+      {userTrips?.map((trip) => (
+        <TripListItem
+          key={trip.trip_id}
+          tripId={trip.trip_id}
+          tripName={trip.trip_name}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default TripList;
